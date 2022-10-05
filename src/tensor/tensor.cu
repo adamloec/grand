c7cdd5a5b7d30e62c032941e19276d478a2daaf3
adamloec <adamloeckle@gmail.com>
2022-10-04 19:44:58 -0500

    Tensor library/dir

--- src/tensor/tensor.cu
+++ src/tensor/tensor.cu
@@ -24,7 +24,7 @@ namespace Grand
         // ===============================================
         // Default tensor constructor, nothing.
         // ===============================================
-        Matrix::Matrix(){}
+        Matrix::Matrix() = default;
 
         // ===============================================
         // Input matrix tensor constructor.
