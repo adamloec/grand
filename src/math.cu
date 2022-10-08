721fdae9259851dfa04431692671aa7dd9022292
adamloec <adamloeckle@gmail.com>
2022-10-08 16:10:07 -0500

    Wrapped math.cu in namespace Grand

--- src/math.cu
+++ src/math.cu
@@ -3,10 +3,6 @@
     #include <math.h>
     #include "math.h"
 #endif
-using namespace Grand;
-
-// Thread block size
-#define BLOCK_SIZE 2
 
 // ===============================================
 // NVIDIA COMPUTE CAPABILITY 8.6 SUPPORTED
@@ -29,98 +25,101 @@ using namespace Grand;
 //
 // ===============================================
 
-// ===============================================
-// Add 2 tensor's kernel function.
-//
-// Tensor::Matrix c = Output tensor
-// Tensor::Matrix a/b = Input tensor's
-// ===============================================
-__global__ void addKernel(Tensor::Matrix c, Tensor::Matrix a, Tensor::Matrix b)
+namespace Grand
 {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-
-    if (i < a.width*a.height)
+    // ===============================================
+    // Add 2 tensor's kernel function.
+    //
+    // Tensor::Matrix c = Output tensor
+    // Tensor::Matrix a/b = Input tensor's
+    // ===============================================
+    __global__ void addKernel(Tensor::Matrix c, Tensor::Matrix a, Tensor::Matrix b)
     {
-        c.tensor[i] = a.tensor[i] + b.tensor[i];
-    }
-}
+        int i = blockDim.x * blockIdx.x + threadIdx.x;
 
-// ===============================================
-// Add 2 tensor's function.
-//
-// Tensor::Matrix c = Output tensor
-// Tensor::Matrix a/b = Input tensor's
-// ===============================================
-cudaError_t add(Tensor::Matrix c, Tensor::Matrix a, Tensor::Matrix b, int device=0)
-{
-    Tensor::Matrix dev_a;
-    Tensor::Matrix dev_b;
-    Tensor::Matrix dev_c;
-    size_t size;
-    cudaError_t cudaStatus;
-
-    // CUDA device check
-    cudaStatus = cudaSetDevice(device);
-    if (cudaStatus != cudaSuccess) 
-    {
-        fprintf(stderr, "ERROR: Cuda enabled device {Device: %d} not found.\n", device);
-        goto Error;
+        if (i < a.width*a.height)
+        {
+            c.tensor[i] = a.tensor[i] + b.tensor[i];
+        }
     }
 
-    // Tensor input dimensions equality check
-    if (a.width != b.width && a.height != b.height && c.width != a.width && c.height != a.height)
+    // ===============================================
+    // Add 2 tensor's function.
+    //
+    // Tensor::Matrix c = Output tensor
+    // Tensor::Matrix a/b = Input tensor's
+    // ===============================================
+    cudaError_t add(Tensor::Matrix c, Tensor::Matrix a, Tensor::Matrix b, int device=0)
     {
-        fprintf(stderr, "ERROR: Tensor dimensions do not match. A: {%d, %d} B: {%d, %d} C: {%d, %d}\n", a.width, a.height, b.width, b.height, c.width, c.height);
-        goto Error;
+        Tensor::Matrix dev_a;
+        Tensor::Matrix dev_b;
+        Tensor::Matrix dev_c;
+        size_t size;
+        cudaError_t cudaStatus;
+
+        // CUDA device check
+        cudaStatus = cudaSetDevice(device);
+        if (cudaStatus != cudaSuccess) 
+        {
+            fprintf(stderr, "ERROR: Cuda enabled device {Device: %d} not found.\n", device);
+            goto Error;
+        }
+
+        // Tensor input dimensions equality check
+        if (a.width != b.width && a.height != b.height && c.width != a.width && c.height != a.height)
+        {
+            fprintf(stderr, "ERROR: Tensor dimensions do not match. A: {%d, %d} B: {%d, %d} C: {%d, %d}\n", a.width, a.height, b.width, b.height, c.width, c.height);
+            goto Error;
+        }
+
+        // Data size (bytes)
+        size = a.width * a.height * sizeof(float);
+
+        // Initialize device tensor's width and height
+        dev_a.width = a.width;
+        dev_a.height = a.height;
+        dev_b.width = b.width;
+        dev_b.height = b.height;
+        dev_c.width = c.width;
+        dev_c.height = c.height;
+        
+        // Device memory allocation for input tensors
+        cudaMalloc(&dev_a.tensor, size);
+        cudaMalloc(&dev_b.tensor, size);
+
+        // Copy input tensor's from host to device memory
+        cudaMemcpy(dev_a.tensor, a.tensor, size, cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_b.tensor, b.tensor, size, cudaMemcpyHostToDevice);
+
+        // Device memory allocation for output tensor
+        cudaMalloc(&dev_c.tensor, size);
+
+        // Invoke kernel with specified kernel dimensions
+        addKernel<<<ceil((a.width*a.height)/256.0), 256>>>(dev_c, dev_a, dev_b);
+
+        // Kernel synchronize, checks for kernel errors
+        cudaStatus = cudaDeviceSynchronize();
+        if (cudaStatus != cudaSuccess) 
+        {
+            fprintf(stderr, "ERROR: Kernel synchronize failed: %d\n", cudaStatus);
+            goto Error;
+        }
+
+        // Copy output tensor from device to host memory
+        cudaStatus = cudaMemcpy(c.tensor, dev_c.tensor, size, cudaMemcpyDeviceToHost);
+        if (cudaStatus != cudaSuccess) 
+        {
+            fprintf(stderr, "ERROR: CUDAMEMCPY: %d\n", cudaStatus);
+            goto Error;
+        }
+
+    Error:
+        cudaFree(dev_c.tensor);
+        cudaFree(dev_a.tensor);
+        cudaFree(dev_b.tensor);
+
+        return cudaStatus;
     }
-
-    // Data size (bytes)
-    size = a.width * a.height * sizeof(float);
-
-    // Initialize device tensor's width and height
-    dev_a.width = a.width;
-    dev_a.height = a.height;
-    dev_b.width = b.width;
-    dev_b.height = b.height;
-    dev_c.width = c.width;
-    dev_c.height = c.height;
-    
-    // Device memory allocation for input tensors
-    cudaMalloc(&dev_a.tensor, size);
-    cudaMalloc(&dev_b.tensor, size);
-
-    // Copy input tensor's from host to device memory
-    cudaMemcpy(dev_a.tensor, a.tensor, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_b.tensor, b.tensor, size, cudaMemcpyHostToDevice);
-
-    // Device memory allocation for output tensor
-    cudaMalloc(&dev_c.tensor, size);
-
-    // Invoke kernel with specified kernel dimensions
-    addKernel<<<ceil((a.width*a.height)/256.0), 256>>>(dev_c, dev_a, dev_b);
-
-    // Kernel synchronize, checks for kernel errors
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) 
-    {
-        fprintf(stderr, "ERROR: Kernel synchronize failed: %d\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output tensor from device to host memory
-    cudaStatus = cudaMemcpy(c.tensor, dev_c.tensor, size, cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) 
-    {
-        fprintf(stderr, "ERROR: CUDAMEMCPY: %d\n", cudaStatus);
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c.tensor);
-    cudaFree(dev_a.tensor);
-    cudaFree(dev_b.tensor);
-
-    return cudaStatus;
 }
 
 
@@ -130,6 +129,7 @@ Error:
 // TO RUN:
 // nvcc math.cu tensor.cu -o math
 // ===============================================
+using namespace Grand;
 int main()
 {
     vector<vector<float>> data{{1, 2}, {3, 4}};
