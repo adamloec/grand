a041f376d228a45cccdbaa2aa398847209fac417
adamloec <adamloeckle@gmail.com>
2023-08-17 00:08:02 -0500

    nn stuff

--- grand/nn.py
+++ grand/nn.py
@@ -3,10 +3,12 @@ from tensor import Tensor
 from activations import Activation
 
 class Dense:
-    def __init__(self, inputs=None, activation=Activation.ReLU):
+    def __init__(self, size, activation=Activation.ReLU):
         self.activation = activation
-        self.inputs = inputs
-        self.dim = inputs.dim
+        self.size = size # 2d array of dim
+    
+    def forward(self, inputs):
+        pass
 
         # create weights and biases, random tensor creation dependent on size of dense layer, weights will be of size (X input, Y output) or Dense(input, neuron)
         # create forward method that calculates logistic regression (z = wx + b), z is output dependent on number of neurons. Ex: Dense(2 input, 3 neuron)
