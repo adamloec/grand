6d599ba110064d5a3090aacfb22d21c206929639
adamloec <adamloeckle@gmail.com>
2023-08-18 11:52:15 -0500

    tensorflow dataset testing

--- tests.py
+++ tests.py
@@ -7,13 +7,36 @@ from grand.nn import Model
 
 
 # model = Model([
-#     Input(2, 3),
+#     Input(4, 3),
 #     Dense(4),
 #     Dense(1)
 #     ])
 
-#model.forward()
+# model.forward()
 
 # Input(batch_size (number of data points), data size)
 # Dense(data size, output size)
 # etc...
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+# print(tf.config.list_physical_devices())
+
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+print("Training initial shape: ", train_images.shape)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(512, activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam', 
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+
+model.summary()
+
+# history = model.fit(train_images, train_labels, epochs=5)
