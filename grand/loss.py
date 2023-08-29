8d5f184e11032c8a8e0ce6609fc0d2546fc71038
adamloec <adamloeckle@gmail.com>
2023-08-29 18:55:42 -0500

    Idk

--- grand/loss.py
+++ grand/loss.py
@@ -1,4 +1,14 @@
 from grand.tensor import Tensor
 
-class CategorialCrossentropy:
-    pass
+
+class CategoricalCrossentropy:
+
+    def _forward(self, prediction, true):
+        if not isinstance(prediction, Tensor):
+            raise TypeError("ERROR: Loss data type must be of type Tensor")
+        if not isinstance(true, Tensor):
+            raise TypeError("ERROR: Loss data type must be of type Tensor")
+        
+        samples = len(prediction.data)
+
+        
